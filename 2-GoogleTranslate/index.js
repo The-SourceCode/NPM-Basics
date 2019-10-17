24ddf8b8bb12dc0f50a6a1aa0d22d9a2d065b150
Yanluis Fermin <tfermin1827@hotmail.com>
2019-10-17 12:59:24 -0400

    Added actual code to episodes 2 & 3

--- 2-GoogleTranslate/index.js
+++ 2-GoogleTranslate/index.js
@@ -1,7 +1,14 @@
-const translate = require("@vitalets/google-translate-api");
+const translate = require('@vitalets/google-translate-api');
 
-translate("Hello World!", {
-    to: "spanish"
-}).then(res => {
-    console.log(res);
-});
+const opts = {
+    to: translate.languages.getCode("spanish"), // Get code of language.
+    from: "en", // Defaults to "auto" which auto detects the language.
+};
+
+translate("Hello World!", opts)
+    .then(response => {
+        console.log(response.text); // Translated text...
+        console.log(response.from.text.value); // Return auto-corrected source text highlighting the issue.
+        console.log(response.from.language.iso); // Translated from...
+    })
+    .catch(console.error);
--- 3-AsciiTable/index.js
+++ 3-AsciiTable/index.js
@@ -1,10 +1,37 @@
-const Table = require("ascii-table");
+const AsciiTable = require('ascii-table');
 
-const example = new Table()
-    .setTitle("Title")
-    .setHeading("AAAAAAAAAAAAAAAAAAAAAAAH")
-    .addRow("HEHE")
-    .setAlignCenter(0);
+const simpleTable = new AsciiTable("Simple Table")
+    .addRow(1, "Earth", "Dead")
+    .addRow(2, "Mars", "Elon Musk");
+console.log(simpleTable.toString());
 
+let i = 0;
+const autoIncrementTable = new AsciiTable("Auto Increment")
+    .addRow(++i, "This is 1", i) // ++i increment first then display number
+    .addRow(++i, "This is 2", i)
+    .addRow(i++, "etc", i); // i++ first display number then increment
+console.log(autoIncrementTable.toString());
 
-console.log(example.toString());
+const noTitle = new AsciiTable()
+    .addRow("What?", "No Title?")
+    .addRow(["What?", "Using arrays?"])
+    .setBorder("*"); // Change border
+console.log(noTitle.toString());
+
+const headingsAlignment = new AsciiTable()
+    .setTitle("Headings")
+    .setHeading("Age", "Name", "Country")
+    .addRow("18", "Jack", "USA")
+    .addRow(["29", "Daeshan", "US of A"])
+    .setAlignCenter(0)
+    .setAlignCenter(1)
+    .setAlign(2, AsciiTable.CENTER);
+console.log(headingsAlignment.toString());
+
+const sorting = new AsciiTable("Sorting Time!").setHeading("Index", "Random #", "Useless");
+for (let i = 0; i < 5; i++) sorting.addRow(i, Math.floor(Math.random() * 10), "Extra");
+console.log(sorting.toString()); // Unsorted table
+sorting.sort((a, b) => b[0] - a[0]); // Sorting by column #1 (descending)
+console.log(sorting.toString());
+sorting.sort((a, b) => a[1] - b[1]); // Sorting by column #2 (ascending)
+console.log(sorting.toString());
